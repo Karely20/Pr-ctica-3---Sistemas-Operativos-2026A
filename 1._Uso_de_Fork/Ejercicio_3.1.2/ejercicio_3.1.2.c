4dc78b3570710f32f8dd7f896f2dbb405d4a3fab
Karely <naomy.bombon@epn.edu.ec>
2026-05-13 00:52:12 +0000

    Ejercicio

--- 1._Uso_de_Fork/Ejercicio_3.1.2/ejercicio_3.1.2.c
+++ 1._Uso_de_Fork/Ejercicio_3.1.2/ejercicio_3.1.2.c
@@ -8,8 +8,9 @@ int main()
 	int pid;
 	int i;
 	int variable = 0;
-	int pipefd[2];
+	int pipefd[2]; 
 
+	//Verifica si falló la creación de pipe
 	if(pipe(pipefd) == -1){
 		perror("Error creando pipe");
 		exit(1);
@@ -26,17 +27,16 @@ int main()
 		case 0: //Proceso hijo
 
 			{
-				close(pipefd[1]);
+				close(pipefd[1]); //El hijo no puede escribir por pipe
 
 				printf("Soy el hijo con el PID: %d\n", getpid());
 
-				FILE *archivo = fopen("log.txt", "w");
+				FILE *archivo = fopen("log.txt", "w"); //Creación de un archivo
 				if(archivo == NULL) exit(1);
-
 				int valor_recibido;
 				for(i = 0; i < 10; i++){
-					read(pipefd[0], &valor_recibido, sizeof(int));
-					fprintf(archivo, "Hijo lee valor de variable: %d\n", valor_recibido);
+					read(pipefd[0], &valor_recibido, sizeof(int)); //El hijo obtiene el valor del padre
+					fprintf(archivo, "Hijo lee valor de variable: %d\n", valor_recibido); //Escribe los datos en el archivo
 				}
 
 				fclose(archivo);
@@ -47,13 +47,13 @@ int main()
 
 		default: //Proceso padre
 
-			close(pipefd[0]);
+			close(pipefd[0]); //El padre no puede leer lo que el mismo envía
 
 			printf("Soy el padre con el PID: %d\n", getpid());
 			for(i = 0; i < 10; i++){
-				variable += 10;
+				variable += 10; //Incrementa una variable
 				printf("Padre incrementa variable a: %d\n", variable);
-				write(pipefd[1], &variable, sizeof(int));
+				write(pipefd[1], &variable, sizeof(int)); //Envìa los valores al hijo
 			}
 
 			close(pipefd[1]);
